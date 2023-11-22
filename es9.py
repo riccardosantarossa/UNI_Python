06d2509d52a78e921e9ed7374684f0261477b47a
Riccardo Santarossa <brudo1968@libero.it>
2023-11-22 10:58:02 +0100

    intervallo con somma data, LINEARE

--- es9.py
+++ es9.py
@@ -3,3 +3,28 @@
 def letturaArray():
     tokens = input().split(" ")
     return [int(x) for x in tokens if x]  # "if x" is for filtering out empty tokens
+
+def findInterval(a, s):
+    
+    i = 0
+    j = 0
+    sommaCorrente = a[j]
+    
+    while i <= j < len(a):  #sommaCorrente = a[i:j+1]
+        
+        if sommaCorrente == s:
+            return i,j
+        elif sommaCorrente < s:
+            j += 1   
+            sommaCorrente += a[j] if j < len(a) else 0  #aggiungo solo se sono nell'array
+        else:
+            i += 1
+            sommaCorrente -= a[i-1] 
+
+    return -1, -1
+
+a = letturaArray()
+s = int(input())
+
+i,j = findInterval(a, s)
+print(i,j)
