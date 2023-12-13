ce9bcbb2f4f4f0a1e4f0ea9ea741fe0c1c74599b
riccardosantarossa <rikysanta03@gmail.com>
2023-12-13 17:51:47 +0100

    .

---
+++ es15.py
@@ -0,0 +1,5 @@
+
+#Calcolare il k-esimo elemento più piccolo in un array dato. 
+#Il programma deve ricevere in input una sequenza di interi separati da spazio e, su una nuova riga, un ulteriore intero positivo k
+# e deve produrre in output il k-esimo elemento più piccolo, ovvero il k-esimo elemento dell'array qualora quest'ultimo fosse ordinato.
+
