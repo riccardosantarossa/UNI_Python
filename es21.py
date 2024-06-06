5c2487a8af19ce2dbb4c81f1058e645757b947bf
riccardosantarossa <rikysanta03@gmail.com>
2024-06-06 15:07:40 +0200

    we did the heights folks

--- es21.py
+++ es21.py
@@ -12,13 +12,14 @@ class Node:
         self.height = 1
     
 def FixHeightUpwInsert(nIndex):
+    
     n = t[nIndex]
     if n.parent is None:
         par = None
     else:
         par = t[n.parent.index] 
 
-    #risalgo l'albero modificando le altezze necessarie - bigO(logn)
+    #risalgo l'albero modificando le altezze necessarie - bigO(n)
     while par is not None:
         if par.left is not None and par.left.key == n.key:
             #n è figlio sinistro
@@ -53,7 +54,7 @@ def FixHeightUpwInsert(nIndex):
  
 #inserimento del nodo al posto giusto
 def insertNode(t, root, k, v):
-    
+
     node = Node(k, v , len(t))
     
     x = root[0]
@@ -115,7 +116,55 @@ def findSuccessor(t, root, key):
     
     return x.index
     
+def FixHeightUpwRemove(nIndex):
+    
+    n = t[nIndex]
+    if n.right is None and n.left is None:
+        n.height = 1
+    elif n.left is None:
+        n.height = n.right.height + 1
+    elif n.right is None:
+        n.height = n.left.height + 1
+    else:
+        n.height = max(n.left.height, n.right.height) + 1
 
+    if n.parent is None:
+        par = None
+    else:
+        par = t[n.parent.index] 
+
+    #risalgo l'albero modificando le altezze necessarie - bigO(n)
+    while par is not None:
+        if par.left is not None and par.left.key == n.key:
+            #n è figlio sinistro
+            if par.right is not None:
+                if par.right.height > n.height:
+                    break
+                else:
+                    par.height = max(n.height, par.right.height) + 1
+            
+            else:
+                #n è l'unico figlio (sinistro)
+                par.height = n.height + 1
+        else:
+            #n è figlio destro
+            if par.left is not None:
+                if par.left.height > n.height:
+                    break
+                else:
+                    par.height = max(par.left.height, n.height) + 1
+
+            else:
+                #n è l'unico figlio (destro)
+                par.height = n.height + 1
+        
+        n = par
+        if par.parent is None:
+            par = None
+        else:
+            par = t[par.parent.index]
+            
+            
 #rimuove il nodo scelto dall'albero
 def removeNode(t, root, key):
     
@@ -140,23 +189,28 @@ def removeNode(t, root, key):
         v.parent = x.parent
     
     if x.parent is not None:
-        
-        pIndex = find(t, root[0], x.parent.key)
-        pNode = t[pIndex]
+
+        pNode = t[x.parent.index]
         
         if x.parent.left is not None:
             if x.key == x.parent.left.key:
                 pNode.left = v
+
             else:
                 pNode.right = v
         else:
             pNode.right = v
+
+        FixHeightUpwRemove(pNode.index)
+
     else:
         root[0] = v
         
     if x.index != z.index:
         z.key = x.key
         z.value = x.value
+
+
    
     
 #MAIN
